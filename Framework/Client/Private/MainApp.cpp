879629cd56a5eccd1b61bf36d65628e36d15a888
Tiny-Prince <rlaxodnjs6574@gmail.com>
2023-12-15 12:45:08 +0900

    Fix : "병합 후 수정."

--- Framework/Client/Private/MainApp.cpp
+++ Framework/Client/Private/MainApp.cpp
@@ -15,7 +15,7 @@
 #include "Light.h"
 
 #ifdef _DEBUG
- #include <vld.h>
+ // #include <vld.h>
 #endif
 
 CMainApp::CMainApp()	
@@ -47,12 +47,12 @@ HRESULT CMainApp::Initialize()
 		return E_FAIL;
 
 	/* 1-4. 게임내에서 사용할 레벨(씬)을 생성한다.   */
-	if (FAILED(Open_Level(LEVEL_TOOL, L"Final_Boss")))
-		return E_FAIL:
+	if (FAILED(Open_Level(LEVEL_TEST, L"Final_Boss")))
+		return E_FAIL;
 
 	// UI Cursor
-	CUI_Manager::GetInstance()->Ready_Cursor();
-	ShowCursor(false);
+	// CUI_Manager::GetInstance()->Ready_Cursor();
+	// ShowCursor(false);
 
 
 	return S_OK;
