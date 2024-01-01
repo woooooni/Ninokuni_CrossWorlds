fa7f97b53dcda6d68fe98386cd76273f0fc44a8d
WonhyeJwa <jade940424@gmail.com>
2024-01-01 21:36:50 +0900

    Fix : "병합 후 수정"

--- Framework/Client/Bin/ShaderFiles/Shader_AnimModel.hlsl
+++ Framework/Client/Bin/ShaderFiles/Shader_AnimModel.hlsl
@@ -887,4 +887,16 @@ technique11 DefaultTechnique
         GeometryShader = NULL;
         PixelShader = compile ps_5_0 PS_SHADOW_DEPTH();
     }
+
+    pass UI_Dummy_NonDepth
+    {
+        // 11
+        SetRasterizerState(RS_CostumeDummy);
+        SetDepthStencilState(DSS_CostumeDummy, 0);
+        SetBlendState(BS_Default, float4(0.f, 0.f, 0.f, 0.f), 0xffffffff);
+
+        VertexShader = compile vs_5_0 VS_MAIN();
+        GeometryShader = NULL;
+        PixelShader = compile ps_5_0 PS_MAIN();
+    }
 };
