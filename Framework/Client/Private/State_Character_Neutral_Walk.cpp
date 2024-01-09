10407110245c70d974be212a547e990fa62ee288
Kim Hana <gksk731@naver.com>
2024-01-09 13:24:05 +0900

    Feature : 오류 수정

--- Framework/Client/Private/State_Character_Neutral_Walk.cpp
+++ Framework/Client/Private/State_Character_Neutral_Walk.cpp
@@ -31,9 +31,7 @@ void CState_Character_Neutral_Walk::Enter_State(void* pArg)
 
 void CState_Character_Neutral_Walk::Tick_State(_float fTimeDelta)
 {
-	GET_INSTANCE(CParticle_Manager)->Tick_Generate_Particle(fTimeDelta, &m_fEffectAcc, CUtils::Random_Float(0.5f, 1.f),
-		TEXT("Particle_Smoke"), m_pCharacter->Get_Component<CTransform>(L"Com_Transform"),
-		_float3(0.f, 0.f, 0.f), _float3(0.f, 0.f, 0.f), _float3(0.f, 0.f, 0.f));
+	GET_INSTANCE(CParticle_Manager)->Tick_Generate_Particle(&m_fEffectAcc, CUtils::Random_Float(0.5f, 1.f), fTimeDelta, TEXT("Particle_Smoke"), m_pCharacter);
 
     __super::Neutral_Walk_Input(fTimeDelta);
     __super::Skill_Input(fTimeDelta);
--- Framework/Engine/Private/Camera_Manager.cpp
+++ Framework/Engine/Private/Camera_Manager.cpp
@@ -37,6 +37,17 @@ void CCamera_Manager::Tick(_float fTimeDelta)
 
 	/* Cur Camera Update */
 	m_pCurCamera->Tick(fTimeDelta);
+
+	/* V(WI) */
+	{
+		GI->Set_Transform(CPipeLine::D3DTS_VIEW, m_pCurCamera->Get_Transform()->Get_WorldMatrixInverse());
+	}
+
+	/* P */
+	{
+		const CCamera::PROJ_DESC& tDesc = m_pCurCamera->Get_ProjDesc();
+		GI->Set_Transform(CPipeLine::D3DTS_PROJ, XMMatrixPerspectiveFovLH(tDesc.tLerpFov.fCurValue, tDesc.fAspect, tDesc.fNear, tDesc.fFar));
+	}
 }
 
 void CCamera_Manager::LateTick(_float fTimeDelta)
@@ -45,19 +56,6 @@ void CCamera_Manager::LateTick(_float fTimeDelta)
 		return;
 
 	m_pCurCamera->LateTick(fTimeDelta);
-
-	/* 아직 보간 처리 X 기본 싱글 카메라만 파이프라인에 전달 */
-
-	/* V(WI) */
-	{
-		GI->Set_Transform(CPipeLine::D3DTS_VIEW, m_pCurCamera->Get_Transform()->Get_WorldMatrixInverse());
-	}
-	
-	/* P */
-	{
-		const CCamera::PROJ_DESC& tDesc = m_pCurCamera->Get_ProjDesc();
-		GI->Set_Transform(CPipeLine::D3DTS_PROJ, XMMatrixPerspectiveFovLH(tDesc.tLerpFov.fCurValue, tDesc.fAspect, tDesc.fNear, tDesc.fFar));
-	}
 }
 
 CCamera* CCamera_Manager::Get_Camera(const _uint& iKey)
