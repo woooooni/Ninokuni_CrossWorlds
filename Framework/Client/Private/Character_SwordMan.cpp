d101d642bd53d7d77862e3dfd198748438b8f9fb
wns2619 <130582634+wns2619@users.noreply.github.com>
2024-01-17 12:05:06 +0900

    Feature JunYeop : SwordMan 렌더그룹 넘는거 주석

--- Framework/Client/Private/Character_SwordMan.cpp
+++ Framework/Client/Private/Character_SwordMan.cpp
@@ -140,7 +140,7 @@ void CCharacter_SwordMan::LateTick(_float fTimeDelta)
 	m_vBloomPower = { 0.0f, 0.0f, 0.7f };
 	__super::LateTick(fTimeDelta);
 
-	m_pRendererCom->Add_RenderGroup(CRenderer::RENDERGROUP::RENDER_REFLECT, this);
+	//m_pRendererCom->Add_RenderGroup(CRenderer::RENDERGROUP::RENDER_REFLECT, this);
 #ifdef DEBUG
 	m_pRendererCom->Add_Debug(m_pControllerCom);
 #endif // DEBUG
