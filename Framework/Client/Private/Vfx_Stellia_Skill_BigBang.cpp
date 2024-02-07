4e6ab2f13bee0ac8de00eb6c1d1b3c184d1ef199
Kim Hana <gksk731@naver.com>
2024-02-07 22:48:45 +0900

    Feature : Stellia BigBang 이펙트 추가

--- Framework/Client/Private/Vfx_Stellia_Skill_BigBang.cpp
+++ Framework/Client/Private/Vfx_Stellia_Skill_BigBang.cpp
@@ -29,10 +29,41 @@ HRESULT CVfx_Stellia_Skill_BigBang::Initialize_Prototype()
 	m_pScaleOffset    = new _float3[m_iMaxCount];
 	m_pRotationOffset = new _float3[m_iMaxCount];
 	
-	m_pFrameTriger[TYPE_ET1_] = 0;
-	m_pPositionOffset[TYPE_ET1_] = _float3(0.f, 0.f, 0.f);
-	m_pScaleOffset[TYPE_ET1_]    = _float3(1.f, 1.f, 1.f);
-	m_pRotationOffset[TYPE_ET1_] = _float3(0.f, 0.f, 0.f);
+	{
+		m_pFrameTriger[TYPE_ET1_E_SMOKE_IN_01] = 4;
+		m_pPositionOffset[TYPE_ET1_E_SMOKE_IN_01] = _float3(0.f, 1.f, 0.f);
+		m_pScaleOffset[TYPE_ET1_E_SMOKE_IN_01]    = _float3(15.f, 1.f, 15.f);
+		m_pRotationOffset[TYPE_ET1_E_SMOKE_IN_01] = _float3(0.f, 0.f, 0.f);
+
+		m_pFrameTriger[TYPE_ET1_P_CIRCLES_IN] = 4;
+		m_pPositionOffset[TYPE_ET1_P_CIRCLES_IN] = _float3(0.f, 1.f, 0.f);
+		m_pScaleOffset[TYPE_ET1_P_CIRCLES_IN]    = _float3(1.f, 1.f, 1.f);
+		m_pRotationOffset[TYPE_ET1_P_CIRCLES_IN] = _float3(0.f, 0.f, 0.f);
+
+		m_pFrameTriger[TYPE_ET1_D_DECAL] = 6;
+		m_pPositionOffset[TYPE_ET1_D_DECAL] = _float3(0.f, 0.f, 0.f);
+		m_pScaleOffset[TYPE_ET1_D_DECAL]    = _float3(30.f, 2.5f, 30.f);
+		m_pRotationOffset[TYPE_ET1_D_DECAL] = _float3(0.f, 0.f, 0.f);
+	}
+
+	{
+		m_pFrameTriger[TYPE_ET2_E_SMOKE_OUT] = 118;
+		m_pPositionOffset[TYPE_ET2_E_SMOKE_OUT] = _float3(0.f, 0.f, -1.f);
+		m_pScaleOffset[TYPE_ET2_E_SMOKE_OUT]    = _float3(1.f, 1.f, 1.f);
+		m_pRotationOffset[TYPE_ET2_E_SMOKE_OUT] = _float3(90.f, 0.f, 0.f);
+
+		m_pFrameTriger[TYPE_ET2_E_SMOKE_UP] = 118;
+		m_pPositionOffset[TYPE_ET2_E_SMOKE_UP] = _float3(0.f, 0.f, 0.f);
+		m_pScaleOffset[TYPE_ET2_E_SMOKE_UP]    = _float3(1.f, 1.f, 1.f);
+		m_pRotationOffset[TYPE_ET2_E_SMOKE_UP] = _float3(0.f, 0.f, 0.f);
+	}
+
+	{
+		m_pFrameTriger[TYPE_ET2_P_CIRCLES_OUTUP] = 120;
+		m_pPositionOffset[TYPE_ET2_P_CIRCLES_OUTUP] = _float3(0.f, 0.f, 0.f);
+		m_pScaleOffset[TYPE_ET2_P_CIRCLES_OUTUP]    = _float3(1.f, 1.f, 1.f);
+		m_pRotationOffset[TYPE_ET2_P_CIRCLES_OUTUP] = _float3(0.f, 0.f, 0.f);
+	}
 
  	return S_OK;
 }
@@ -48,8 +79,61 @@ void CVfx_Stellia_Skill_BigBang::Tick(_float fTimeDelta)
 
 	if (!m_bOwnerTween)
 	{
-		if (m_iCount == TYPE_ET1_ && m_iOwnerFrame >= m_pFrameTriger[TYPE_ET1_])
+		if (m_iCount == TYPE_ET1_E_SMOKE_IN_01 && m_iOwnerFrame >= m_pFrameTriger[TYPE_ET1_E_SMOKE_IN_01])
 		{
+			GET_INSTANCE(CEffect_Manager)->Generate_Effect(TEXT("Effect_Stellia_BigBang_Smoke_In"),
+				XMLoadFloat4x4(&m_WorldMatrix), m_pPositionOffset[TYPE_ET1_E_SMOKE_IN_01], m_pScaleOffset[TYPE_ET1_E_SMOKE_IN_01], m_pRotationOffset[TYPE_ET1_E_SMOKE_IN_01], nullptr, &m_pSmokeIn_01, false);
+			Safe_AddRef(m_pSmokeIn_01);
+			m_iCount++;
+		}
+		else if (m_iCount == TYPE_ET1_P_CIRCLES_IN && m_iOwnerFrame >= m_pFrameTriger[TYPE_ET1_P_CIRCLES_IN])
+		{
+			GET_INSTANCE(CParticle_Manager)->Generate_Particle(TEXT("Particle_Stellia_Skill_BigBang_Circles_In"),
+				XMLoadFloat4x4(&m_WorldMatrix), m_pPositionOffset[TYPE_ET1_P_CIRCLES_IN], m_pScaleOffset[TYPE_ET1_P_CIRCLES_IN], m_pRotationOffset[TYPE_ET1_P_CIRCLES_IN], nullptr, &m_pCircleInParticle, false);
+			Safe_AddRef(m_pCircleInParticle);
+			m_iCount++;
+		}
+		else if (m_iCount == TYPE_ET1_D_DECAL && m_iOwnerFrame >= m_pFrameTriger[TYPE_ET1_D_DECAL])
+		{
+			GET_INSTANCE(CEffect_Manager)->Generate_Decal(TEXT("Decal_Stellia_BigBang_Decal"),
+				XMLoadFloat4x4(&m_WorldMatrix), m_pPositionOffset[TYPE_ET1_D_DECAL], m_pScaleOffset[TYPE_ET1_D_DECAL], m_pRotationOffset[TYPE_ET1_D_DECAL], nullptr, &m_pDecal, false);
+			Safe_AddRef(m_pDecal);
+			m_iCount++;
+		}
+		
+		else if (m_iCount == TYPE_ET2_E_SMOKE_OUT && m_iOwnerFrame >= m_pFrameTriger[TYPE_ET2_E_SMOKE_OUT])
+		{
+			CEffect* Smoke_01 = nullptr;
+			GET_INSTANCE(CEffect_Manager)->Generate_Effect(TEXT("Effect_Stellia_BigBang_Smoke_Out"),
+				XMLoadFloat4x4(&m_WorldMatrix), m_pPositionOffset[TYPE_ET2_E_SMOKE_OUT], m_pScaleOffset[TYPE_ET2_E_SMOKE_OUT], m_pRotationOffset[TYPE_ET2_E_SMOKE_OUT], nullptr, &Smoke_01);
+			if (nullptr != Smoke_01)
+			{
+				Smoke_01->Set_ScaleSpeed(10.f);
+				Smoke_01->Set_Color(_float3(0.482f, 0.157f, 1.f));
+			}
+
+			CEffect* Smoke_02 = nullptr;
+			GET_INSTANCE(CEffect_Manager)->Generate_Effect(TEXT("Effect_Stellia_BigBang_Smoke_Out"),
+				XMLoadFloat4x4(&m_WorldMatrix), m_pPositionOffset[TYPE_ET2_E_SMOKE_OUT], m_pScaleOffset[TYPE_ET2_E_SMOKE_OUT], m_pRotationOffset[TYPE_ET2_E_SMOKE_OUT], nullptr, &Smoke_02);
+			if (nullptr != Smoke_02)
+			{
+				Smoke_02->Set_ScaleSpeed(15.f);
+			}
+			m_iCount++;
+		}
+		else if (m_iCount == TYPE_ET2_E_SMOKE_UP && m_iOwnerFrame >= m_pFrameTriger[TYPE_ET2_E_SMOKE_UP])
+		{
+			GET_INSTANCE(CEffect_Manager)->Generate_Effect(TEXT("Effect_Stellia_JumpStamp_Shock01"),
+				XMLoadFloat4x4(&m_WorldMatrix), m_pPositionOffset[TYPE_ET2_E_SMOKE_UP], m_pScaleOffset[TYPE_ET2_E_SMOKE_UP], m_pRotationOffset[TYPE_ET2_E_SMOKE_UP]);
+			m_iCount++;
+		}
+		else if (m_iCount == TYPE_ET2_P_CIRCLES_OUTUP && m_iOwnerFrame >= m_pFrameTriger[TYPE_ET2_P_CIRCLES_OUTUP])
+		{
+			GET_INSTANCE(CParticle_Manager)->Generate_Particle(TEXT("Particle_Stellia_Skill_BigBang_CircleCircle"),
+				XMLoadFloat4x4(&m_WorldMatrix), m_pPositionOffset[TYPE_ET2_P_CIRCLES_OUTUP], m_pScaleOffset[TYPE_ET2_P_CIRCLES_OUTUP], m_pRotationOffset[TYPE_ET2_P_CIRCLES_OUTUP]);
+			GET_INSTANCE(CParticle_Manager)->Generate_Particle(TEXT("Particle_Stellia_Skill_BigBang_CircleCircle_02"),
+				XMLoadFloat4x4(&m_WorldMatrix), m_pPositionOffset[TYPE_ET2_P_CIRCLES_OUTUP], m_pScaleOffset[TYPE_ET2_P_CIRCLES_OUTUP], m_pRotationOffset[TYPE_ET2_P_CIRCLES_OUTUP]);
+
 			m_iCount++;
 		}
 
@@ -102,6 +186,22 @@ CGameObject* CVfx_Stellia_Skill_BigBang::Clone(void* pArg)
 void CVfx_Stellia_Skill_BigBang::Free()
 {
 	__super::Free();
+
+	if (nullptr != m_pSmokeIn_01)
+	{
+		m_pSmokeIn_01->Set_UVLoop(1);
+		Safe_Release(m_pSmokeIn_01);
+	}
+	if (nullptr != m_pCircleInParticle)
+	{
+		m_pCircleInParticle->Set_LoopParticle(false);
+		Safe_Release(m_pCircleInParticle);
+	}
+	if (nullptr != m_pDecal)
+	{
+		m_pDecal->Start_AlphaDeleate();
+		Safe_Release(m_pDecal);
+	}
 
 	if (!m_isCloned)
 	{
