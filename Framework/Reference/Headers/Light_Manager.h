00578931c090d26207f1e726d527829c87b545d0
Kim Hana <gksk731@naver.com>
2024-02-13 02:53:49 +0900

    Feature : UI 메시용 라이트 추가 및 렌더러에 적용

--- Framework/Reference/Headers/Light_Manager.h
+++ Framework/Reference/Headers/Light_Manager.h
@@ -13,32 +13,37 @@ public:
 	virtual ~CLight_Manager() = default;
 
 public:
+	HRESULT Reset_Lights();
+
+	// World
+	list<class CLight*>* Get_LightList() { return &m_Lights; }
 	const LIGHTDESC* Get_LightDesc(_uint iIndex);
 
-public:
-	HRESULT Add_Light(ID3D11Device* pDevice, ID3D11DeviceContext* pContext, const LIGHTDESC& LightDesc);
+	XMMATRIX GetLightViewMatrix();
 
-	HRESULT Add_ShadowLight(_uint iLevelIndex, _vector vEye, _vector vAt, _vector vUp);
-	HRESULT Set_ShadowLight(_uint iLevelIndex, _vector vEye, _vector vAt, _vector vUp);
-
-	HRESULT Add_ShadowProj(_uint iLevelIndex, _float fFovAngleY, _float fAspectRatio, _float fNearZ, _float fFarZ);
-
-	HRESULT Render(class CShader* pShader, class CVIBuffer_Rect* pVIBuffer);
-
-
-public:
 	_float4x4 Get_ShadowLightViewMatrix(_uint iLevelIndex);
 	_float4x4 Get_ShadowLightViewMatrix_Inverse(_uint iLevelIndex);
-
 	_float4x4 Get_ShadowLightProjMatrix(_uint iLevelIndex);
 
-	XMMATRIX GetLightViewMatrix();
 public:
-	list<class CLight*>* Get_LightList() { return &m_Lights; }
-
+	// World
+	//-> Light 
+	HRESULT Add_Light(ID3D11Device* pDevice, ID3D11DeviceContext* pContext, const LIGHTDESC& LightDesc);
+	//-> Shadow
+	HRESULT Add_ShadowLight(_uint iLevelIndex, _vector vEye, _vector vAt, _vector vUp);
+	HRESULT Set_ShadowLight(_uint iLevelIndex, _vector vEye, _vector vAt, _vector vUp);
+	HRESULT Add_ShadowProj(_uint iLevelIndex, _float fFovAngleY, _float fAspectRatio, _float fNearZ, _float fFarZ);
+	//-> Render
+	HRESULT Render(class CShader* pShader, class CVIBuffer_Rect* pVIBuffer);
 
 public:
-	HRESULT Reset_Lights();
+	// UI
+	//-> Light 
+	HRESULT Add_Light_UI(ID3D11Device* pDevice, ID3D11DeviceContext* pContext, const LIGHTDESC& LightDesc);
+	//-> Render
+	HRESULT Render_UI(class CShader* pShader, class CVIBuffer_Rect* pVIBuffer);
+
+public:
 	HRESULT	Add_Sun(class CGameObject* pSun);
 
 	void SetMainSun(_bool appear) { m_bDisappear = appear; }
@@ -47,16 +52,18 @@ public:
 	Vec4 Get_SunScreenPos();
 	const class CGameObject* Get_Sun() const { return m_pSun; }
 
-	
-
 private:
-	list<class CLight*>			m_Lights;
+	list<class CLight*> m_Lights;
 	typedef list<class CLight*>	LIGHTS;
-	class  CGameObject* m_pSun = nullptr;
 
-private:
 	map<_uint, _float4x4> m_ShadowViewMatrix;
 	map<_uint, _float4x4> m_ShadowProjMatrix;
+
+private:
+	list<class CLight*> m_Lights_UI;
+
+private:
+	class  CGameObject* m_pSun = nullptr;
 
 private:
 	_bool m_bDisappear = false;
